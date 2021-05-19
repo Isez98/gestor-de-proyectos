d4ebb9ffbb018682c1fabc2e0b97e21e39d922fe
diegocorp <diegosandovalr.corp@gmail.com>
2021-05-19 16:49:50 -0700

    Fixed connection collision bug

--- gpi-server/Controller/AWS/index.js
+++ gpi-server/Controller/AWS/index.js
@@ -1,66 +1,70 @@
-const aws = require('aws-sdk');
+const aws = require("aws-sdk");
 const dotenv = require("dotenv");
 dotenv.config();
 const multer = require("multer");
 const multers3 = require("multer-s3");
 const s3 = new aws.S3({
-  region: 'us-west-1',
-})
+  region: "us-west-1",
+});
 
-const uploadImage = multer({ 
-   storage: multers3({
-     s3: s3,
-     bucket: 'gpi-images',
-     acl: 'public-read',
-     contentType: multers3.AUTO_CONTENT_TYPE,
-     metadata: function(req, file, cb){ 
-       cb(null, { fieldName: file.fieldname });
-     },
-     key: function(req, res, cb){
-       let fullpath = `users/${req.body.imageName}`;
-       cb(null, fullpath)
-     }
-   })
+const uploadImage = multer({
+  storage: multers3({
+    s3: s3,
+    bucket: "gpi-images",
+    acl: "public-read",
+    contentType: multers3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, res, cb) {
+      let fullpath = `users/${req.body.imageName}`;
+      cb(null, fullpath);
+    },
+  }),
 });
 
 const getImage = (req, res) => {
-  try{
+  try {
     const fullpath = `users/${req.params.fileName}`;
-    const params = { Bucket: "gpi-images", Key: fullpath, Expires: 60*5 };
-    const url = s3.getSignedUrl('getObject',params)
-    res.status(200).json({success:true, data: url})
-  }catch(error) {console.log(error.message)}  
-} 
+    const params = { Bucket: "gpi-images", Key: fullpath, Expires: 60 * 5 };
+    const url = s3.getSignedUrl("getObject", params);
+    res.status(200).json({ success: true, data: url });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const uploadDocument = multer({
   storage: multers3({
     s3,
-    bucket: 'gpi-images',
-    acl: 'public-read',
+    bucket: "gpi-images",
+    acl: "public-read",
     contentType: multers3.AUTO_CONTENT_TYPE,
-    metadata: function(req, file, cb){
+    metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
-    key: function(req, res, cb){
+    key: function (req, res, cb) {
       console.log(req.body);
       let fullpath = `projects/${req.params.id}/${req.body.fileName}`;
-      cb(null, fullpath)
-    }
-  })
+      cb(null, fullpath);
+    },
+  }),
 });
 
 const getDocument = (req, res) => {
   try {
     const fullpath = `projects/${req.params.id}/${req.params.fileName}`;
-    const params = { Bucket: 'gpi-images', Key: fullpath, Expires: 60*5 };
-    const url = s3.getSignedUrl('getObject', params);
-    res.status(200).json({success:true, data:url});
-  } catch (error) { console.log(error.message); }
-}
+    const params = { Bucket: "gpi-images", Key: fullpath, Expires: 60 * 5 };
+    const url = s3.getSignedUrl("getObject", params);
+    res.status(200).json({ success: true, data: url });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports = { 
-  uploadImage, 
+module.exports = {
+  uploadImage,
   getImage,
   uploadDocument,
-  getDocument 
-}
+  getDocument,
+};
--- gpi-server/Controller/User/index.js
+++ gpi-server/Controller/User/index.js
@@ -1,43 +1,42 @@
 const { response } = require("express");
 const Project = require("../../Database/Models/Project");
-const User = require('../../Database/Models/User');
+const User = require("../../Database/Models/User");
 const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
 createUser = async (req, res) => {
-
-const user = new User({
+  const user = new User({
     userName: req.body.userName,
     email: req.body.email,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     academy: req.body.academy,
     employeeNumber: req.body.employeeNumber,
-    password: req.body.password
-});
+    password: req.body.password,
+  });
 
-if(!user){
-  return res.status(404).json({ status: false, error: err });
-}
+  if (!user) {
+    return res.status(404).json({ status: false, error: err });
+  }
 
-await user.save()
-.then(() => {
-  return res.status(201).json({
-    success: true,
-    id: user._id,
-    message: 'User created.'
-  })
-}).catch(error => {
-  return res.status(400).json({
-    error,
-    message: 'User not created. Data received: ', user
-  })
+  await user
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: user._id,
+        message: "User created.",
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: "User not created. Data received: ",
+        user,
+      });
+    });
 
-}); 
-
-
-
-/*   const body = req.body;
+  /*   const body = req.body;
 
   if(!body){
     return res.status(404).json({
@@ -65,26 +64,26 @@ await user.save()
         message: 'User not created. Data received: ', user
       })
     }); */
-}
+};
 
 updateUser = async (req, res) => {
   const body = req.body;
 
-  if (!body){
+  if (!body) {
     return res.status(400).json({
       success: false,
-      error: "You must provide a body to update."
+      error: "You must provide a body to update.",
     });
   }
 
   await User.findOne({ _id: req.params.id }, (err, user) => {
-    if(err){
+    if (err) {
       return res.status(404).json({
-        err, 
-        message: 'Usuario no encontrado',
-      })
+        err,
+        message: "Usuario no encontrado",
+      });
     }
-    
+
     user.userName = body.userName;
     user.email = body.email;
     user.firstName = body.firstName;
@@ -94,110 +93,114 @@ updateUser = async (req, res) => {
     user.password = body.password;
     user.image = body.image;
 
-    user.save()
+    user
+      .save()
       .then(() => {
         return res.status(200).json({
           success: true,
           id: user._id,
-          message: 'User updated!'
-        })
-      }).catch(error => {
+          message: "User updated!",
+        });
+      })
+      .catch((error) => {
         return res.status(404).json({
           error,
-          message: 'User not updated'
-        })
-      })
-  })
-}
+          message: "User not updated",
+        });
+      });
+  });
+};
 
 deleteUser = async (req, res) => {
   await User.findOneAndDelete({ _id: req.params.id }, (err, user) => {
     if (err) {
       return res.status(400).json({
         success: false,
-        error: err
+        error: err,
       });
     }
-    if(!user){
-      return res.status(404)
-        .json({ success: false, error: 'User not found.' });
+    if (!user) {
+      return res.status(404).json({ success: false, error: "User not found." });
     }
-    return res.status(200).json({ success: true, data: user })
-  }).catch(err => console.log(err))
-}
+    return res.status(200).json({ success: true, data: user });
+  }).catch((err) => console.log(err));
+};
 
 getUserByEmail = async (req, res) => {
   await User.findOne({ email: req.params.id }, (err, user) => {
-    if(err){
-      return res.status(400).json({ success: false, error: err })
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
     }
 
-    if(!user){
-      return res.status(404).json({ success: false, error: 'User not found' })
+    if (!user) {
+      return res.status(404).json({ success: false, error: "User not found" });
     }
-    return res.status(200).json({ success: true, data: user })
-  }).catch(err => console.log(err));
-}
+    return res.status(200).json({ success: true, data: user });
+  }).catch((err) => console.log(err));
+};
 
 getUsers = async (req, res) => {
   await User.find({}, (err, users) => {
-    if(err){
-      return res.status(400).json({ success: false, error: err })
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
     }
-    if(!users.length){
-      return res.status(400).json({ success: false, error: 'Project not found' })
+    if (!users.length) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Project not found" });
     }
-    return res.status(200).json({ success: true, data: users })
-  }).catch(err => console.log(err))
-}
+    return res.status(200).json({ success: true, data: users });
+  }).catch((err) => console.log(err));
+};
 
 login = async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     return res.status(400).json({
-      errors: errors.array()
+      errors: errors.array(),
     });
   }
-  const {email, password} = req.body;
-  try{
-    let user = await User.findOne({ email }).catch(err => console.log(err));
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email }).catch((err) => console.log(err));
 
-    if(!user){
+    if (!user) {
       return res.status(400).json({
-        message: "Usuario no existe..."
-      })
+        message: "Usuario no existe...",
+      });
     }
 
     //Use bcrypt compare when transitioning to encryption
-    if(password !== user.password){
+    if (password !== user.password) {
       return res.status(400).json({
-        message: "Error in el ingreso de usuario!"
+        message: "Error in el ingreso de usuario!",
       });
     }
 
     const payload = {
       user: {
-        id: user.id
-      }
+        id: user.id,
+      },
     };
 
     jwt.sign(
       payload,
-      "randomString", {
-        expiresIn: 100000
+      "randomString",
+      {
+        expiresIn: 100000,
       },
       (err, token) => {
-        if(err) throw err;
+        if (err) throw err;
         res.status(200).json({
-          token
+          token,
         });
       }
     );
-  } catch(err){
+  } catch (err) {
     console.log(err.message);
-    res.status(500).send("Error en guardar")
+    res.status(500).send("Error en guardar");
   }
-}
+};
 
 module.exports = {
   createUser,
@@ -205,5 +208,5 @@ module.exports = {
   deleteUser,
   getUserByEmail,
   getUsers,
-  login
-}
+  login,
+};
